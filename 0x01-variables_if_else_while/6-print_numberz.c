2d3b096370ffd10cd8d56f5c71d112046db646e4
nelly_siwe <nelly.fumile@gmail.com>
2023-08-14 08:39:06 -0700

    this is my seventh commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - main function
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number;
+for (number = 0; number <= 9; number++)
+{
+putchar(number + '0');
+}
+putchar('\n');
+return (0);
+}
+
