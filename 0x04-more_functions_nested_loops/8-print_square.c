ec0e519ad2354e492e36926bbfc678ef27671966
nelly_siwe <nelly.fumile@gmail.com>
2023-08-17 13:55:22 -0700

    This is my ninth commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_square - prints a squares
+*@size: parameter
+*Return:returns nothing
+*/
+void print_square(int size)
+{
+int a,b;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < (size - 1); b++)
+_putchar('#');
+
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
