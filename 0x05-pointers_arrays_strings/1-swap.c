b9c88ab023d2cfd4e49c171417a173c4f6dd3074
nelly_siwe <nelly.fumile@gmail.com>
2023-08-21 05:07:00 -0700

    this is the second commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * swap_int - swap two integers
- * @a: value 1
- * @b: value 2
- * Return: void
- */
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
-	int c:
-
-	c = *a;
-	*a = *b;
-	*b = c;
+int tmp = *a;
+*a = *b;
+*b = tmp;
 }
