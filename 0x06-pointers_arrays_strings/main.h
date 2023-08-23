2bc5a4549fe41a81cad489abdc94c18cf0defdcf
nelly_siwe <nelly.fumile@gmail.com>
2023-08-23 01:10:23 -0700

    this is the first commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
+char *_strcat(char *dest, char *src);
+#endif /* MAIN_H */
