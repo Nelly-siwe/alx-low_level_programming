e078f5f6c8474dd0a66fcbaa3d2806e82bd2c751
nelly_siwe <nelly.fumile@gmail.com>
2023-08-31 04:51:54 -0700

    THis is my 1st commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <_putchar.c>
 /**
 *void prototypes(void)
 *int prototypes(int)
@@ -10,5 +9,5 @@
 */
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-
+int _putchar(char c);
 #endif /* MAIN_H */
